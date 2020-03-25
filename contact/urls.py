9446ff252b823dac9f706fe5cf7ddf086e07d7ac
innorev14 <tokyo3@netsgo.com>
2020-03-25 14:59:24 +0900

    feat/ applied bulma css

--- contact/urls.py
+++ contact/urls.py
@@ -6,7 +6,7 @@ app_name = 'contact'
 
 urlpatterns = [
     path('', views.ContactListView.as_view(), name='email_list'),
-    path('create/', views.ContactCreateView.as_view(), name='email_send'),
+    path('create/', views.ContactCreateView.as_view(), name='email_create'),
     path('update/<pk>', views.ContactUpdateView.as_view(), name='email_update'),
     path('delete/<pk>', views.ContactDeleteView.as_view(), name='email_delete'),
     path('detail/<pk>', views.ContactDetailView.as_view(), name='email_detail'),
--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, DeleteView, UpdateView
 
 from .models import Contact
@@ -10,6 +11,7 @@ class ContactListView(ListView):
 
 class ContactCreateView(CreateView):
     model = Contact
+    fields = '__all__'
     template_name = 'contact/contact_create.html'
 
 
@@ -19,7 +21,9 @@ class ContactDetailView(DetailView):
 
 class ContactUpdateView(UpdateView):
     model = Contact
+    fields = '__all__'
 
 
 class ContactDeleteView(DeleteView):
     model = Contact
+    success_url = reverse_lazy('cotact:email_list')
