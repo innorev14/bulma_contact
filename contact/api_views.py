c9b0668ae4fa50ce2a12414cc27c58c921d69934
innorev14 <tokyo3@netsgo.com>
2020-03-30 14:33:47 +0900

    fix/ api doc

--- contact/api_views.py
+++ contact/api_views.py
@@ -3,5 +3,17 @@ from .serializers import ContactSerializer
 from .models import Contact
 
 class ContactAPIView(generics.ListCreateAPIView):
+    '''
+        Contact 리스트를 보여주거나 생성하는 API
+
+        ---
+        # 내용
+            - name : 이름
+            - email : 이메일
+            - phone_no : 전화번호
+            - service : 문의유형
+            - url : 홈페이지 주소
+            - content : 내용
+    '''
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
--- contact/serializers.py
+++ contact/serializers.py
@@ -2,18 +2,6 @@ from rest_framework import serializers
 from .models import Contact
 
 class ContactSerializer(serializers.ModelSerializer):
-    '''
-        Contact 리스트를 보여주거나 생성하는 API
-
-        ---
-        # 내용
-            - name : 이름
-            - email : 이메일
-            - phone_no : 전화번호
-            - service : 문의유형
-            - url : 홈페이지 주소
-            - content : 내용
-    '''
     class Meta:
         model = Contact
         fields = '__all__'
